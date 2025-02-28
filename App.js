a62bc65c4b03aa0da69d0587f41dc5211404370a
Rodrigo Kauling <rodrigo765@hotmail.com>
2025-02-28 00:00:43 -0300

    Teste de alteração

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Text } from 'react-native';
 export default function App() {
   return <>
       <Text>Jornada JS!</Text>
-      <Text>Hello Word!</Text>
+      <Text>Hello Word 22!</Text>
   </>
 }
 
